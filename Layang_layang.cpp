42950f37396181faf53a4db7bb8b919863945cf2
M. A Z <alfinyuki@gmail.com>
2020-08-28 19:45:37 +0700

    C-Dasar

---
+++ Layang_layang.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include  <math.h>
+
+	int main(){
+		int nilai_a;
+		int nilai_b;
+		float hasil;
+		printf ("Nilai a=");scanf("%i",&nilai_a);
+		printf ("Nilai b=");scanf("%i",&nilai_a);
+		hasil= sqrt((nilai_a*nilai_a)+pow(nilai_a,2));
+		printf("Hasil= %f\n",hasil);
+	}
---
+++ Luas Lingkaran.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+	int main(){
+		int jari2;
+		float phi = 3.14;
+		float luas;
+			printf("input nilai jari-jari=");
+			scanf ("%i",& jari2);
+			luas=(jari2*jari2)*phi;
+			printf("jari jari=%i,luas=%f",jari2,luas);
+			return 1;
+			
+			
+	}
